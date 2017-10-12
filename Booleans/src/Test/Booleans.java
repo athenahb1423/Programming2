4e1d82ddc1d508c488e50b8e02e7267e678271b6
mhs <mhs@MHS-Science2-15>
2017-10-11 17:02:34 -0700

    Second Program

---
+++ Booleans/src/Test/Booleans.java
@@ -0,0 +1,20 @@
+package Test;
+
+	public class Booleans {
+		public static void main(String[] args) {
+					int y=9;
+					int x=16;
+					int c=5;
+					int n=0;
+			while (n<3) {
+				System.out.println ("Go Programming");
+				n++;
+			}
+	
+			while (y<9) {
+				System.out.println ("Go Programming");
+				n++;
+			}
+			
+		}
+	}
